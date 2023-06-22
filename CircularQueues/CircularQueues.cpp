03523e313fc6a5b6c7339e16ee1a48fa72559382
RamaSandhy <rshandy07@gmail.com>
2023-06-22 10:40:53 +0700

    membuat public dan mengecek  apakah antrian penuh

--- CircularQueues/CircularQueues.cpp
+++ CircularQueues/CircularQueues.cpp
@@ -4,4 +4,22 @@ using namespace std;
 class Queues {
 	int FRONT, REAR, max = 5;
 	int queues_array[5];
-}
+
+public:
+	Queues() {
+		FRONT = -1;
+		REAR = -1;
+	}
+
+	void insert() {
+		int num;
+		cout << "Enter a number: ";
+		cin >> num;
+		cout << endl;
+
+		// Cek apakah antrian penuh
+		if ((FRONT == 0 && REAR == max - 1) || (FRONT == REAR + 1)) {
+			cout << "\nQueue overflow\n";
+		}
+	}
+};
